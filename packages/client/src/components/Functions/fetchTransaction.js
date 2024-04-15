3a974746f461185695a55451c34ae6725f697587
AyushWaghe <144901247+AyushWaghe@users.noreply.github.com>
2024-04-15 10:32:24 +0530

    Update fetchTransaction.js

--- packages/client/src/components/Functions/fetchTransaction.js
+++ packages/client/src/components/Functions/fetchTransaction.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchTransactions = async (userName) => {
     try {
-      const response = await axios.get(`http://localhost:3001/fetchTransactions/fetchG?userName=${userName}`);
+      const response = await axios.get(`https://financemvc.onrender.com/fetchTransactions/fetchG?userName=${userName}`);
       console.log("erert");
       // console.log(response.data.transactions);
       return response.data.transactions;
