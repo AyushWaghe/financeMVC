a9331ef1c6267f737723f200bc9d30f7af93cc30
AyushWaghe <ayushwaghe23@gmail.com>
2024-04-15 15:38:31 +0530

    News update
    news

--- packages/client/src/components/APINews/Main.js
+++ packages/client/src/components/APINews/Main.js
@@ -6,22 +6,28 @@ import SideNavBar from '../SideNavBar/SideNavBar';
 
 const Main = () => {
   const [navBarisToggle, setNavBarisToggle] = useState(false);
-
   const [articles, setArticles] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
-
   const setNavBarTogggle = () => {
     setNavBarisToggle(!navBarisToggle);
-  }
+  };
 
   const fetchNews = async () => {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/everything?q=finance&apiKey=22b4c4e6191f45abb125735bb94a1c2d&page=${currentPage}`);
-      console.log(response.data.articles[0].url);
-      // Set the new articles, replacing the existing ones
-      setArticles(response.data.articles.slice(0, 6));
-      setCurrentPage(currentPage + 1);
+      const response = await axios.get(
+        `https://saurav.tech/NewsAPI/top-headlines/category/business/in.json`
+      );
+
+      // Calculate the start and end index for the slice based on the current page
+      const startIndex = (currentPage - 1) * 6;
+      const endIndex = currentPage * 6;
+
+      // Set the new articles based on the slice
+      setArticles(response.data.articles.slice(startIndex, endIndex));
+
+      // Update the current page number, cycling back to 1 if it exceeds 10
+      setCurrentPage((currentPage % 10) + 1);
     } catch (error) {
       console.log(error);
     }
@@ -29,28 +35,31 @@ const Main = () => {
 
   useEffect(() => {
     fetchNews();
-  }, []);
+  }, []); // Trigger fetchNews when currentPage changes
 
   return (
     <div className="SuperMasterContainer">
       <div className="navBar">
-        <SideNavBar
-          isToggle={setNavBarTogggle}
-        />
+        <SideNavBar isToggle={setNavBarTogggle} />
       </div>
 
       {navBarisToggle && <div className="Model"></div>}
-      <div className='Master'>
-      <div className="NewsPageHeading">
-        Today's Finance feed!
-      </div>
+      <div className="Master">
+        <div className="NewsPageHeading">Today's Finance feed!</div>
         <div className="NewsCardContainer">
           {articles.map((article, index) => (
-            <News key={index} title={article.title} author={article.author} sourceName={article.source.name} url={article.url} />
+            <News
+              key={index}
+              title={article.title}
+              author={article.author}
+              sourceName={article.source.name}
+              url={article.url}
+            />
           ))}
-
         </div>
-        <button class="fetch-button" onClick={fetchNews}>Next</button>
+        <button className="fetch-button" onClick={fetchNews}>
+          Next
+        </button>
       </div>
     </div>
   );
